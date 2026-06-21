8865a190d821803aa536ecbc49eb19262bdeedd3
Rimi-Li <surim980404@gmail.com>
2026-06-21 10:06:19 +0900

    Add end_session debug log
    Add debug logging for session tracking

--- study_bot.py
+++ study_bot.py
@@ -174,6 +174,8 @@ def get_time(user_id, channel, today_only=False):
 
 
 async def end_session(member):
+    print("END SESSION:", member.display_name)
+    
     global last_log
 
     session = active_sessions.pop(member.id, None)
