9c93190ba17985bde505bfd4b385430396bf4648
Rimi-Li <surim980404@gmail.com>
2026-06-22 12:03:33 +0900

    Check test

--- study_bot.py
+++ study_bot.py
@@ -174,6 +174,7 @@ def get_time(user_id, channel, today_only=False):
 
 
 async def end_session(member):
+    print("END SESSION")
     global last_log
 
     session = active_sessions.pop(member.id, None)
